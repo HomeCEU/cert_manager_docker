9f2881554d56bfa6338ae4ba590dc208aefbfd9f
tempcke <t.github@logikostech.net>
2020-01-15 12:45:29 -0600

    implemented ./app.sh init

--- app.sh
+++ app.sh
@@ -9,6 +9,7 @@ Usage: app.sh COMMAND
 
 Commands:
   help|--help    Display this help
+  init           clone application, composer install
   composer       execute any composer command on the app
 EOF
   exit 0;
@@ -22,8 +23,13 @@ function Composer() {
   if [ ${cmd} = "install" ]; then
     args="${args} --no-suggest --prefer-dist";
   fi
-  echo "${cmd} ${args}";
+  echo "composer ${cmd} ${args}";
   docker run --rm -v "${DIR}/${APP_DIR}":/app --user ${uid}:${gid} composer:1.8 $cmd $args
+}
+
+function Init() {
+  git clone --branch develop git@github.com:HomeCEU/certificate-manager.git ${APP_DIR}
+  Composer install
 }
 
 function route() {
@@ -33,6 +39,9 @@ function route() {
       ;;
     composer)
       Composer "${@:2}"
+      ;;
+    init)
+      Init
       ;;
   esac
 }
